ed36ee7d8ac75ed11363791002dda2e0e4fc227e
Mickaël Salaün <mic@digikod.net>
2019-11-04 17:33:10 +0100

    landlock: Add the management of domains
    A Landlock domain is a set of eBPF programs.  There is a list for each
    different program types that can be run on a specific Landlock hook
    (e.g. ptrace).  A domain is tied to a set of subjects (i.e. tasks).  A
    Landlock program should not try (nor be able) to infer which subject is
    currently enforced, but to have a unique security policy for all
    subjects tied to the same domain.  This make the reasoning much easier
    and help avoid pitfalls.
    
    The next commits tie a domain to a task's credentials thanks to
    seccomp(2), but we could use cgroups or a security file-system to
    enforce a sysadmin-defined policy .
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: James Morris <jmorris@namei.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Serge E. Hallyn <serge@hallyn.com>
    Cc: Will Drewry <wad@chromium.org>
    ---
    
    Changes since v12:
    * move the landlock_put_domain() call from landlock_prepend_prog() to
      domain_syscall.c:landlock_seccomp_prepend_prog() (next commit): keep
      the same semantic but don't conditionally own the domain (safeguard
      domain owning)
    * always copy a domain to-be-modified which guarantee domain
      immutability: the callers of landlock_prepend_prog() are now fully in
      charge of concurrency handling, which is currently only handled by
      prepare_creds() and commit_creds() (next commit)
    * simplify landlock_prepend_prog()
    * use a consistent naming
    
    Changes since v11:
    * remove old code from previous refactoring (removing the program
      chaining concept) and simplify program prepending (reported by Serge
      E. Hallyn):
      * simplify landlock_prepend_prog() and merge it with
        store_landlock_prog()
      * add new_prog_list() and rework new_landlock_domain()
      * remove the extra page allocation checks, only rely on the eBPF
        program checks
    * replace the -EINVAL for the duplicate program check with the -EEXIST
    
    Changes since v10:
    * rename files and names to clearly define a domain
    * create a standalone patch to ease review

--- security/landlock/common.h
+++ security/landlock/common.h
@@ -11,10 +11,48 @@
 
 #include <linux/bpf.h>
 #include <linux/filter.h>
+#include <linux/refcount.h>
 
 enum landlock_hook_type {
 	LANDLOCK_HOOK_PTRACE = 1,
 };
+
+#define _LANDLOCK_HOOK_LAST LANDLOCK_HOOK_PTRACE
+
+struct landlock_prog_list {
+	struct landlock_prog_list *prev;
+	struct bpf_prog *prog;
+	refcount_t usage;
+};
+
+/**
+ * struct landlock_domain - Landlock programs enforced on a set of tasks
+ *
+ * When prepending a new program, if &struct landlock_domain is shared with
+ * other tasks, then duplicate it and prepend the program to this new &struct
+ * landlock_domain.
+ *
+ * @usage: reference count to manage the object lifetime. When a task needs to
+ *	   add Landlock programs and if @usage is greater than 1, then the
+ *	   task must duplicate &struct landlock_domain to not change the
+ *	   children's programs as well.
+ * @programs: array of non-NULL &struct landlock_prog_list pointers
+ */
+struct landlock_domain {
+	struct landlock_prog_list *programs[_LANDLOCK_HOOK_LAST];
+	refcount_t usage;
+};
+
+/**
+ * get_hook_index - get an index for the programs of struct landlock_prog_set
+ *
+ * @type: a Landlock hook type
+ */
+static inline size_t get_hook_index(enum landlock_hook_type type)
+{
+	/* type ID > 0 for loaded programs */
+	return type - 1;
+}
 
 static inline enum landlock_hook_type get_hook_type(const struct bpf_prog *prog)
 {
